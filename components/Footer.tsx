ef1d97943d152b58e9a66f810c435ae1d3372322
Arun Acharya <113877370+arunacharya1603@users.noreply.github.com>
2025-10-31 13:04:01 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,7 +42,10 @@ const Footer = () => {
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              onClick={() => window.open(info.link, "_blank")}
+              onClick={() =>
+                typeof window !== "undefined" &&
+                window.open(info.link, "_blank")
+              }
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <Image
