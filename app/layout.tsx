afab674a64f035e366c1fbc4010e97d888d81ef9
arunacharya1603 <arunacharya1603@gmail.com>
2026-06-25 11:27:08 +0530

    feat: update favicon and manifest with new icons and formats for improved compatibility and visual appeal

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,10 +104,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
-        <link rel="shortcut icon" href="/favicon.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#0e0d0c" />
         <link
           rel="alternate"
--- app/manifest.ts
+++ app/manifest.ts
@@ -14,12 +14,12 @@ export default function manifest(): MetadataRoute.Manifest {
     lang: "en",
     icons: [
       {
-        src: "/favicon.png",
+        src: "/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/favicon.png",
+        src: "/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
